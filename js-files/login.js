b439f9f9c0ea30f0f24ea7784a57812fcb699129
Michael Calhoun <michael.calhoun91@gmail.com>
2014-08-10 15:08:01 -0500

    HTML5
    Converted site to HTML5

--- js-files/login.js
+++ js-files/login.js
@@ -1,15 +1,6 @@
-$( document ).ready(function() {
- 
-    alert( "Welcome!" );
- 
-});
+$(document).ready( function codeagreementpopup() {
 
-$(document).ready( function() {
- 
-$("#confirm_box").click( function() {
-jConfirm('Please confirm this?', 'Confirmation Dialog', function(r) {
-jAlert('Click Result: ' + r, 'Visitor Response');
-});
-});
- 
+if (confirm("Press OK to state that you will not reuse the code of this site without the authors permission") == true) {}
+
+else { window.location.assign("http://www.w3schools.com/js/default.asp")}
 });
